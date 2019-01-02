88d936849cff9b6f903577d93772c45f2c826655
Geunyoung Kim <kgysmart@gmail.com>
2019-01-02 14:33:57 -0800

    Add hello world

---
+++ 01-hello-world/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
+
+/*
+
+$ go run main.go
+Hello world
+
+$ go build main.go
+$ ls
+main main.go
+
+$ ./main
+Hello world
+
+*/
