eca330871b9cf2f9a2b7b15af5402949071ae170
Geunyoung Kim <kgysmart@gmail.com>
2019-01-11 10:02:39 -0800

    Add comment

--- 35-rate-limiting/rate-limiting.go
+++ 35-rate-limiting/rate-limiting.go
@@ -2,6 +2,8 @@ package main
 
 /*
 
+https://gobyexample.com/rate-limiting
+
 Rate Limiting is an important mechamism for controlling
 resource utilization and maintaining quality of service.
 Go elegantly supports rate limiting with goroutines, channels and tickers.
@@ -24,25 +26,48 @@ func main() {
 	}
 	close(requests)
 
+	/*
+		The limiter channel will receive a value very 200 milliseconds.
+		This is the regulator in our rate limiting scheme.
+	*/
 	limiter := time.Tick(200 * time.Millisecond)
 
+	/*
+		By blocking on a receive from the limiter channel before serving each request,
+		we limit ourselves to 1 request every 200 milliseconds.
+	*/
 	for req := range requests {
 		<-limiter
 		fmt.Println("request", req, time.Now())
 	}
 
+	/*
+		We may want to allow short bursts of requests in our rate limiting scheme while preserving
+		the overall rate limit. We can accomplish this by buffering our limiter channel.
+		This burstyLimiter channel will allow bursts of up to 3 events.
+	*/
 	burstyLimiter := make(chan time.Time, 3)
 
+	/*
+		Fill up the channel to represent allowed bursting.
+	*/
 	for i := 0; i < 3; i++ {
 		burstyLimiter <- time.Now()
 	}
 
+	/*
+		Every 200 milliseconds we'll try to add a new value to burstyLimiter, up to its limit of 3.
+	*/
 	go func() {
 		for t := range time.Tick(200 * time.Millisecond) {
 			burstyLimiter <- t
 		}
 	}()
 
+	/*
+		Now simulate 5 more incoming requests. The first 3 of these will benefit
+		from the burst from the burst capability of burstyLimter.
+	*/
 	burstyRequests := make(chan int, 5)
 	for i := 1; i <= 5; i++ {
 		burstyRequests <- i
