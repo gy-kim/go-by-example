5b645bb5fa22b8c911a3ca2a9997892b59f77959
Geunyoung Kim <kgysmart@gmail.com>
2019-01-11 09:58:18 -0800

    Add Rate Limiting

---
+++ 35-rate-limiting/rate-limiting.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+
+Rate Limiting is an important mechamism for controlling
+resource utilization and maintaining quality of service.
+Go elegantly supports rate limiting with goroutines, channels and tickers.
+
+*/
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	/*
+		First we'll look at basic rate limiting. Support we want to
+		limit our handling of incoming requests. We'll serve these requests off a channel of the same name
+	*/
+	requests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		requests <- i
+	}
+	close(requests)
+
+	limiter := time.Tick(200 * time.Millisecond)
+
+	for req := range requests {
+		<-limiter
+		fmt.Println("request", req, time.Now())
+	}
+
+	burstyLimiter := make(chan time.Time, 3)
+
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(200 * time.Millisecond) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
+}
